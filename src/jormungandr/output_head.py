81d54aadcfa114c85e555665fe9cc8aee8fee2e6
Sverre Nystad <sverrenystad@gmail.com>
2026-02-26 13:23:21 +0100

    feat: add MLPPredictionHead class for bounding box predictions

---
+++ src/jormungandr/output_head.py
@@ -0,0 +1,19 @@
+import torch.nn as nn, Tensor
+
+class MLPPredictionHead(nn.Module):
+    """
+    Very simple multi-layer perceptron (MLP, also called FFN), used to predict the normalized center coordinates,
+    height and width of a bounding box w.r.t. an image.
+
+    """
+
+    def __init__(self, input_dim: int, hidden_dim: int, output_dim: int, num_layers: int):
+        super().__init__()
+        self.num_layers = num_layers
+        h = [hidden_dim] * (num_layers - 1)
+        self.layers = nn.ModuleList(nn.Linear(n, k) for n, k in zip([input_dim] + h, h + [output_dim]))
+
+    def forward(self, x: Tensor) -> Tensor:
+        for i, layer in enumerate(self.layers):
+            x = nn.functional.relu(layer(x)) if i < self.num_layers - 1 else layer(x)
+        return x
