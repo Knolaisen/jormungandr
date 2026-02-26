26430df668ed6ad2348056c1375fbbb3be2de875
Sverre Nystad <sverrenystad@gmail.com>
2026-02-26 11:11:20 +0100

    feat: remove unused code

--- tests/test_example.py
+++
@@ -1,8 +0,0 @@
-
-
-def test_example():
-    """
-    Example test function to demonstrate the structure of a test.
-    This function should be replaced with actual test logic.
-    """
-    assert True
