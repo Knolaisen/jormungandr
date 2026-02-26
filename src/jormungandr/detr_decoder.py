e880306b465504519335fa9879aecf4b5de020e0
Sverre Nystad <sverrenystad@gmail.com>
2026-02-26 13:23:08 +0100

    feat: implement DETRDecoder class with query embedding

---
+++ src/jormungandr/detr_decoder.py
@@ -0,0 +1,9 @@
+from torch import nn, Tensor
+
+
+class DETRDecoder(nn.Module):
+    def __init__(self, num_queries: int = 16, hidden_dim: int = 16):
+        super(DETRDecoder, self).__init__()
+        self.num_queries = num_queries
+        self.query_embed = nn.Embedding(num_queries, hidden_dim)
+        # Additional layers can be added here
