05b0e02343d073e54e02d28c022d0f57998f9ebb
Sverre Nystad <sverrenystad@gmail.com>
2026-03-05 10:22:05 +0100

    feat: add config to wandb tracking

--- train.py
+++ train.py
@@ -10,9 +10,13 @@ from jormungandr.training.trainer import train
 
 
 if __name__ == "__main__":
-    wandb.login(key=WANDB_API_KEY)
-    wandb.init(project=WANDB_PROJECT, entity=WANDB_ENTITY)
-
     config = load_config("config.yaml")
+    wandb.login(key=WANDB_API_KEY)
+    wandb.init(
+        project=WANDB_PROJECT,
+        entity=WANDB_ENTITY,
+        # mode="disabled",
+        config=config.model_dump(),
+    )
 
     train(config)
