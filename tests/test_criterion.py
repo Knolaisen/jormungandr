908769d64c9551d863a9a3641f38a4ef6e33ce21
Sverre Nystad <sverrenystad@gmail.com>
2026-03-12 09:43:43 +0100

    test: add test case for zero area bounding box

--- tests/test_criterion.py
+++ tests/test_criterion.py
@@ -12,25 +12,22 @@ from transformers.loss.loss_for_object_detection import (
 @pytest.mark.parametrize(
     "pred_boxes",
     [
-        torch.tensor([[0, 0, -1, -1]]),  # Bounding box completely outside the image
-        torch.tensor(
-            [[0.5, 0.5, -0.001, -0.001]]
-        ),  # Bounding box completely outside the image
-        torch.tensor([[-1, -1, 0.1, 0.1]]),  # Bounding box completely outside the image
-        torch.tensor(
-            [[0.5, 0.5, 0.2, 0.2]]
-        ),  # Bounding box completely inside the image
-        torch.tensor(
-            [[1.5, 1.5, 0.2, 0.2]]
-        ),  # Bounding box completely outside the image
+        # Bounding box completely outside the image
+        torch.tensor([[0, 0, -1, -1]]),
+        # Bounding box with zero area at the top-left corner
+        torch.tensor([[0, 0, 0, 0]]),
+        # Bounding box completely outside the image
+        torch.tensor([[0.5, 0.5, -0.001, -0.001]]),
+        # Bounding box completely outside the image
+        torch.tensor([[-1, -1, 0.1, 0.1]]),
+        # Bounding box completely inside the image
+        torch.tensor([[0.5, 0.5, 0.2, 0.2]]),
+        # Bounding box completely outside the image
+        torch.tensor([[1.5, 1.5, 0.2, 0.2]]),
     ],
 )
 def test_bounding_box_out_of_image(pred_boxes):
-
-    # Test case 1: Bounding box completely outside the image
-    # giou_cost = -generalized_box_iou(center_to_corners_format(out_bbox), center_to_corners_format(target_bbox))
-
-    # pred_boxes = torch.tensor([[0, 0, -1, -1]])  # (x_center, y_center, width, height)
+    # pred_boxes (x_center, y_center, width, height)
     target_boxes = torch.tensor(
         [[0.5, 0.5, 0.2, 0.2]]
     )  # (x_center, y_center, width, height)
